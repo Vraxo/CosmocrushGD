bb09f44ebed2e9274e0244a6b6119159bf32ae73
Parsa <Parsa@DESKTOP-32S8QLS>
2025-04-09 18:46:19 +0330

    FAILED

--- Scripts/World.cs
+++ Scripts/World.cs
@@ -1,29 +1,34 @@
-﻿using Godot;
+using Godot;
 
 namespace Cosmocrush;
 
 public partial class World : WorldEnvironment
 {
-    [Export] private PackedScene pauseMenuScene;
+	[Export] private PackedScene pauseMenuScene;
 
-    private PauseMenu pauseMenu;
+	private PauseMenu pauseMenu;
 
-    public override void _Ready()
-    {
-        pauseMenu = GetNode<PauseMenu>("/root/World/Player/Camera2D/PauseMenu");
-    }
+	public override void _Ready()
+	{
+		pauseMenu = GetNode<PauseMenu>("/root/World/Player/Camera2D/PauseMenu");
+	}
 
-    public override void _Process(double delta)
-    {
-        if (Input.IsKeyPressed(Key.Space) && !GetTree().Paused)
-        {
-            Pause();
-        }
-    }
+	public override void _Process(double delta)
+	{
+		if (Input.IsKeyPressed(Key.Space) && !GetTree().Paused)
+		{
+			Pause();
+		}
+	}
 
-    private void Pause()
-    {
-        GetTree().Paused = true;
-        pauseMenu.Show();
-    }
+	private void Pause()
+	{
+		GetTree().Paused = true;
+		pauseMenu.Show();
+	}
+
+	private void _on_pause_button_pressed()
+	{
+		Pause();
+	}
 }
