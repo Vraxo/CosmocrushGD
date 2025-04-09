f0b381c764cfbb0d3aecba77a7b8dcdf74c7ff2f
Parsa <Parsa@DESKTOP-32S8QLS>
2025-04-09 19:03:50 +0330

    Added pause menu and made particles orange

--- Scripts/World.cs
+++ Scripts/World.cs
@@ -1,29 +1,37 @@
-﻿using Godot;
+using Godot;
 
 namespace Cosmocrush;
 
 public partial class World : WorldEnvironment
 {
-    [Export] private PackedScene pauseMenuScene;
+	[Export] private PackedScene pauseMenuScene;
+	[Export] private Button pauseButton;
 
-    private PauseMenu pauseMenu;
+	private PauseMenu pauseMenu;
 
-    public override void _Ready()
-    {
-        pauseMenu = GetNode<PauseMenu>("/root/World/Player/Camera2D/PauseMenu");
-    }
+	public override void _Ready()
+	{
+		pauseMenu = GetNode<PauseMenu>("/root/World/Player/Camera2D/PauseMenu");
 
-    public override void _Process(double delta)
-    {
-        if (Input.IsKeyPressed(Key.Space) && !GetTree().Paused)
-        {
-            Pause();
-        }
-    }
+		pauseButton.Pressed += OnPauseButtonPressed;
+	}
 
-    private void Pause()
-    {
-        GetTree().Paused = true;
-        pauseMenu.Show();
-    }
+	public override void _Process(double delta)
+	{
+		if (Input.IsKeyPressed(Key.Space) && !GetTree().Paused)
+		{
+			Pause();
+		}
+	}
+
+	private void OnPauseButtonPressed()
+	{
+		Pause();
+	}
+
+	private void Pause()
+	{
+		GetTree().Paused = true;
+		pauseMenu.Show();
+	}
 }
